edb204b23b6d7fef71ff4d52f7709eff5109be91
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-05-08 18:59:56 +0200

    Update input_number.py

--- custom_components/piscinexa/input_number.py
+++ custom_components/piscinexa/input_number.py
@@ -30,10 +30,10 @@ class PiscinexaInputNumber(InputNumber):
         self._type = type_
         self._attr_unique_id = f"{entry.entry_id}_{type_}"
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, f"piscinexa_{name}")},
-            name=name.capitalize(),
+            identifiers={(DOMAIN, f"piscinexa_{name}_{type_}")},
+            name=f"Input {type_.replace('_', ' ').capitalize()}",
             manufacturer="Piscinexa",
-            model="Piscine",
+            model="Input Numérique",
             sw_version="1.0.2",
         )
         self._attr_value = default_val
@@ -48,6 +48,7 @@ class PiscinexaInputNumber(InputNumber):
             f"entity.input_number.piscinexa_{self._type}.name",
             self._type.replace("_", " ").capitalize()
         )
+        self._attr_device_info.name = self._attr_name
 
     async def async_set_value(self, value: float) -> None:
         self._attr_value = value
