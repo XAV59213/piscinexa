8d697e6ce5c9330bb2d22075ce8d1e4f9e25c53f
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-05-08 13:14:40 +0200

    Update input_select.py

--- custom_components/piscinexa/input_select.py
+++ custom_components/piscinexa/input_select.py
@@ -22,16 +22,24 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
 class PiscinexaPhPlusTreatmentSelect(InputSelect):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry, name: str):
-        super().__init__(
-            options=["Liquide", "Granulés"],
-            name=f"{name}_ph_plus_treatment",
-        )
+        unique_id = f"{entry.entry_id}_ph_plus_treatment"
+        super().__init__(config={
+            "id": unique_id,
+            "name": f"{name}_ph_plus_treatment",
+            "options": [
+                hass.helpers.template.render("entity.input_select.{name}_ph_plus_treatment.options.liquid", {"name": name}),
+                hass.helpers.template.render("entity.input_select.{name}_ph_plus_treatment.options.granules", {"name": name}),
+            ],
+        })
         self._hass = hass
         self._entry = entry
         self._name = name
         self._attr_name = f"{name}_ph_plus_treatment"
-        self._attr_friendly_name = f"{name.capitalize()} Type de traitement pH+"
-        self._attr_unique_id = f"{entry.entry_id}_ph_plus_treatment"
+        self._attr_friendly_name = hass.helpers.template.render(
+            "entity.input_select.{name}_ph_plus_treatment.name",
+            {"name": name.capitalize()}
+        )
+        self._attr_unique_id = unique_id
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, f"piscinexa_{name}")},
             name=name.capitalize(),
@@ -40,21 +48,29 @@ class PiscinexaPhPlusTreatmentSelect(InputSelect):
             sw_version="1.0.2",
         )
         self._attr_icon = "mdi:water-plus"
-        self._attr_value = entry.data.get("ph_plus_treatment", "Liquide")
-        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_value)
+        self._attr_current_option = self._entry.data.get("ph_plus_treatment", "Liquide")
+        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_current_option)
 
 class PiscinexaPhMinusTreatmentSelect(InputSelect):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry, name: str):
-        super().__init__(
-            options=["Liquide", "Granulés"],
-            name=f"{name}_ph_minus_treatment",
-        )
+        unique_id = f"{entry.entry_id}_ph_minus_treatment"
+        super().__init__(config={
+            "id": unique_id,
+            "name": f"{name}_ph_minus_treatment",
+            "options": [
+                hass.helpers.template.render("entity.input_select.{name}_ph_minus_treatment.options.liquid", {"name": name}),
+                hass.helpers.template.render("entity.input_select.{name}_ph_minus_treatment.options.granules", {"name": name}),
+            ],
+        })
         self._hass = hass
         self._entry = entry
         self._name = name
         self._attr_name = f"{name}_ph_minus_treatment"
-        self._attr_friendly_name = f"{name.capitalize()} Type de traitement pH-"
-        self._attr_unique_id = f"{entry.entry_id}_ph_minus_treatment"
+        self._attr_friendly_name = hass.helpers.template.render(
+            "entity.input_select.{name}_ph_minus_treatment.name",
+            {"name": name.capitalize()}
+        )
+        self._attr_unique_id = unique_id
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, f"piscinexa_{name}")},
             name=name.capitalize(),
@@ -63,21 +79,30 @@ class PiscinexaPhMinusTreatmentSelect(InputSelect):
             sw_version="1.0.2",
         )
         self._attr_icon = "mdi:water-minus"
-        self._attr_value = entry.data.get("ph_minus_treatment", "Liquide")
-        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_value)
+        self._attr_current_option = self._entry.data.get("ph_minus_treatment", "Liquide")
+        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_current_option)
 
 class PiscinexaChloreTreatmentSelect(InputSelect):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry, name: str):
-        super().__init__(
-            options=["Chlore choc (poudre)", "Pastille lente", "Liquide"],
-            name=f"{name}_chlore_treatment",
-        )
+        unique_id = f"{entry.entry_id}_chlore_treatment"
+        super().__init__(config={
+            "id": unique_id,
+            "name": f"{name}_chlore_treatment",
+            "options": [
+                hass.helpers.template.render("entity.input_select.{name}_chlore_treatment.options.shock", {"name": name}),
+                hass.helpers.template.render("entity.input_select.{name}_chlore_treatment.options.tablet", {"name": name}),
+                hass.helpers.template.render("entity.input_select.{name}_chlore_treatment.options.liquid", {"name": name}),
+            ],
+        })
         self._hass = hass
         self._entry = entry
         self._name = name
         self._attr_name = f"{name}_chlore_treatment"
-        self._attr_friendly_name = f"{name.capitalize()} Type de traitement Chlore"
-        self._attr_unique_id = f"{entry.entry_id}_chlore_treatment"
+        self._attr_friendly_name = hass.helpers.template.render(
+            "entity.input_select.{name}_chlore_treatment.name",
+            {"name": name.capitalize()}
+        )
+        self._attr_unique_id = unique_id
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, f"piscinexa_{name}")},
             name=name.capitalize(),
@@ -86,5 +111,5 @@ class PiscinexaChloreTreatmentSelect(InputSelect):
             sw_version="1.0.2",
         )
         self._attr_icon = "mdi:water-check"
-        self._attr_value = entry.data.get("chlore_treatment", "Chlore choc (poudre)")
-        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_value)
+        self._attr_current_option = self._entry.data.get("chlore_treatment", "Chlore choc (poudre)")
+        _LOGGER.debug("Entité input_select %s créée avec valeur initiale %s", self._attr_name, self._attr_current_option)
