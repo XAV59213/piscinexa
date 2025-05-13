2a394e0918ac2b0d9229cf459ad3f426c5266035
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-05-13 17:19:48 +0200

    Update const.py

--- custom_components/piscinexa/const.py
+++ custom_components/piscinexa/const.py
@@ -2,17 +2,15 @@
 
 DOMAIN = "piscinexa"
 
+# Version de l'intégration
+VERSION = "1.0.13"
+
 # Types de piscine
-CONF_POOL_TYPE = "pool_type"
 POOL_TYPE_SQUARE = "square"
 POOL_TYPE_ROUND = "round"
 
-# Clés de configuration
-CONF_NAME = "name"
-CONF_LENGTH = "length"
-CONF_WIDTH = "width"
-CONF_DEPTH = "depth"
-CONF_DIAMETER = "diameter"
+# Constantes mathématiques
+PI = 3.141592653589793
 
 # Unités de mesure
 UNIT_CUBIC_METERS = "m³"
@@ -21,8 +19,9 @@ UNIT_LITERS = "L"
 UNIT_GRAMS = "g"
 UNIT_MG_PER_LITER = "mg/L"
 
-# Constante mathématique
-PI = 3.14159265359
-
-# Version de l'intégration
-VERSION = "1.0.11"
+# Clés de configuration
+CONF_POOL_TYPE = "pool_type"
+CONF_LENGTH = "length"
+CONF_WIDTH = "width"
+CONF_DEPTH = "depth"
+CONF_DIAMETER = "diameter"
